4378503b795e5be24ea9a917665b3e45a2e406e6
Shawn O'Hare <shawn@140proof.com>
2015-03-17 17:47:31 -0700

    Add Max function to stats.  Change output.
    The Min/Max functions now return the index of mininmal/maximal
    object as well.

--- stats.go
+++ stats.go
@@ -1,5 +1,6 @@
 // Statistical operations with respect the mass.  Values are assumed
 // to be in the extended real numbers (which includes +, - infinty).
+// When necessary, pre-sorting is assumed.
 package mass
 
 import (
@@ -26,18 +27,46 @@ func Mean(data Interface) float64 {
 	return mean
 }
 
-// Min returns the minimal mass from a not necessarily sorted collection.
-func Min(data Interface) float64 {
+// Min returns the minimal mass and corresponding index.
+func Min(data Interface) (float64, int) {
 	// For finite sets the minimal value is equal to the
 	// greatest lower bound, or infimum. The infimum for
 	// the empty set is vacuously positive infinity.
 	min := math.Inf(1)
+	index := -1
 	if data.Len() > 0 {
 		min = data.Mass(0)
+		index = 0
 		for i := 1; i < data.Len(); i++ {
-			min = math.Min(min, data.Mass(i))
+			mass := data.Mass(i)
+			if mass < min {
+				index = i
+				min = mass
+			}
 		}
 	}
 
-	return min
+	return min, index
+}
+
+// Max returns the maximal mass and corresponding index.
+func Max(data Interface) (float64, int) {
+	// For finite sets the maximal value is equal to the
+	// least upper bound, or supremum.  The supremum for
+	// the empty set is vacuously negative infinity.
+	max := math.Inf(-1)
+	index := -1
+	if data.Len() > 0 {
+		max := data.Mass(0)
+		index = 0
+		for i := 1; i < data.Len(); i++ {
+			mass := data.Mass(i)
+			if mass > max {
+				index = i
+				max = mass
+			}
+		}
+	}
+
+	return max, index
 }
