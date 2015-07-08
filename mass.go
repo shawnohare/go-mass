dce5e6c21802fcbb6e0763153705d85c8fc9ea25
Shawn O'Hare <ohare.shawn@gmail.com>
2015-07-07 18:41:07 -0700

    Refactor.

--- mass.go
+++ mass.go
@@ -1,12 +1,13 @@
 // Package mass provides interfaces and function to perform
 // set and statistical  operations on data that has some notion of mass
-// norm.  Examples of such data include:
+//  Examples of such data include:
 //
-// - planets and their masses.
-// - functions and their norms.
-// - a collection of random variables and an associated response variable.
+// - Planets and their masses.
+// - Functions and their norms.
+// - A collection of random variables and an associated response variable.
+// - A key, numeric value pair.
 //
-// A typical structure implementing the mass.Interface is a slice
+// A typical structure implementing the mass.Collection interface is a slice
 // []A, where A is a type with some attribute corresponding to a real number.
 // This attribute then induces a total ordering on []A that we might wish to:
 //
@@ -19,26 +20,19 @@ import (
 	"sort"
 )
 
-// TODO consider removing dependency on sort interface.
-// We will never use this explicitly if we demand the data has been
-// previously sorted.
-
-// A type, typically a collection, that satisfies mass.Interface
-// can be partitioned/sorted/selected from by the functions in this package.
-// The  methods require that the elements can be enumerated by an integer index
-// and assigned an numeric value.
-type Interface interface {
+// A Collection is a type that can sorted/partitioned/fetched from and
+// represents an ordered set of objects that can be mapped to a real number.
+type Collection interface {
 	// Len is the number of elements in a the collection
 	Len() int
-	// Less reports whether the element with index i should sort before
-	// element with index j.  It induces a total ordering on the collection.
-	// Typically Less(i, j) = Mass(i) < Mass(j).
-	// Less(i, j int) bool
-	// Swap swaps the elements with indexes i and j
-	// Swap(i, j int) bool
-	// Mass reports the mass/norm/value of the element indexed by i.
+	// Get the ith element.
+	Get(i int) interface{}
+	// Get the ith element's mass.
 	Mass(i int) float64
 }
 
-// NOTE All the methods that follow assume the data has been previously
-// sorted by mass.
+func Sort(col Collection) Collection {
+	c := NewList(col)
+	sort.Sort(c)
+	return c
+}
