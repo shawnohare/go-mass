254914bb545ae790eacea345b4c94e7394eeab4c
Shawn O'Hare <ohare.shawn@gmail.com>
2015-07-09 09:01:28 -0700

    Move partition methods to a separate package.  (Again).

--- mpart/partition.go
+++ mpart/partition.go
@@ -1,30 +1,32 @@
-package mass
-
-import (
-	"errors"
-	"math"
-)
-
-// A Partition of a Collection interface into cells.
+// Package mpart provides functions that partition mass.mass.Collection interfaces.
 // We always view the collection, via its natural embedding, as a discrete
 // subset of the reals.  All partition definitions are actually a family
 // of sub-interval cell partitions that induce the same discrete partition
 // over the collection.  That is, individual partition functions
 // return a list of slice points for the collection that defines
 // the partition.
+package mpart
+
+import (
+	"errors"
+	"github.com/shawnohare/go-mass"
+	"math"
+)
+
+// A Partition of a mass.Collection interface into cells.
 type Partition struct {
 	// Slice points that define the partition.
 	Indices []int
 	// Points on the Real line that induce the partition.
 	Points []float64
 	// The actual partition of the collection into discrete cells.
-	Cells []Slice
+	Cells []mass.Slice
 }
 
 // Make produces a slice of sub-collections according
 // to the input partition indices.  This function assumes that the
 // collection has been previously sorted.
-func MakePartition(c Collection, partition []int) (*Partition, error) {
+func Make(c mass.Collection, partition []int) (*Partition, error) {
 	var err error
 
 	cells, err := makeCells(c, partition)
@@ -39,15 +41,15 @@ func MakePartition(c Collection, partition []int) (*Partition, error) {
 	return p, err
 }
 
-func makeCells(c Collection, partition []int) ([]Slice, error) {
+func makeCells(c mass.Collection, partition []int) ([]mass.Slice, error) {
 	if len(partition) < 2 || c.Len() < len(partition)-1 {
 		err := errors.New("Could not make cells.")
 		return nil, err
 	}
 
-	s := MakeSlice(c)
+	s := mass.MakeSlice(c)
 	numCells := len(partition) - 1
-	ss := make([]Slice, numCells)
+	ss := make([]mass.Slice, numCells)
 	for i := 0; i < numCells; i++ {
 		start, end := partition[i], partition[i+1]
 		ss[i] = s[start:end]
@@ -58,7 +60,7 @@ func makeCells(c Collection, partition []int) ([]Slice, error) {
 
 // makePoints a slice-index partition into
 // a sub-interval partition whose cells have maximum width.
-func makePoints(c Collection, par []int) []float64 {
+func makePoints(c mass.Collection, par []int) []float64 {
 	points := make([]float64, len(par))
 	for i, idx := range par {
 		if idx == c.Len() {
@@ -72,19 +74,19 @@ func makePoints(c Collection, par []int) []float64 {
 
 // MakeEqualWidthCells produces a Partition instance with populated cells
 // defined by the EqualWidthCells partition.
-func MakeEqualWidthCells(c Collection, n int) (*Partition, error) {
+func MakeEqualWidthCells(c mass.Collection, n int) (*Partition, error) {
 	return MakeMinSizeCells(c, n, 0)
 }
 
 // EqualWidthCells partitions the collection into equal width subintervals.
 // It is the partial evaluation of MinSizedCells.
-func DefineEqualWidthCells(c Collection, n int) (*Partition, error) {
+func DefineEqualWidthCells(c mass.Collection, n int) (*Partition, error) {
 	return DefineMinSizeCells(c, n, 0)
 }
 
 // MakeMinSizedCells calls MinSizedCells to obtain a partition
 // definition and then makes the specified partition.
-func MakeMinSizeCells(c Collection, n, k int) (*Partition, error) {
+func MakeMinSizeCells(c mass.Collection, n, k int) (*Partition, error) {
 	p, err := DefineMinSizeCells(c, n, k)
 	if err != nil {
 		return nil, err
@@ -95,14 +97,14 @@ func MakeMinSizeCells(c Collection, n, k int) (*Partition, error) {
 }
 
 // MinSizeCells produces a partition of the previously sorted
-// input Collection into n subinterval cells each of which has at least
+// input mass.Collection into n subinterval cells each of which has at least
 // k elements. The partition tries to be as close to an equal width
 // partition as possible. In particular, when k = 0, the result is
 // an equal width partition.
-func DefineMinSizeCells(c Collection, n, k int) (*Partition, error) {
+func DefineMinSizeCells(c mass.Collection, n, k int) (*Partition, error) {
 	N := c.Len()
 	if n < 1 || k < 0 || N < n*k || N < 2 || c.Mass(N-1) == c.Mass(0) {
-		err := errors.New("Collection cannot be partitioned.")
+		err := errors.New("mass.Collection cannot be partitioned.")
 		return nil, err
 	}
 
--- mpart/partition_test.go
+++ mpart/partition_test.go
@@ -1,19 +1,20 @@
-package mass
+package mpart
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/shawnohare/go-mass"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMakePartition(t *testing.T) {
 	var err error
-	var s Slice
+	var s mass.Slice
 	// A partition variable.
 
 	// Test a basic two cell equal width partition.
-	s = Slice{
+	s = mass.Slice{
 		{0, 1},
 		{1, 2.2},
 		{2, 8.2},
@@ -25,12 +26,12 @@ func TestMakePartition(t *testing.T) {
 	assert.Equal(t, []float64{1, 5, 9}, partition.Points)
 
 	// Ensure that Make* and Make produce the same output.
-	p2, err := MakePartition(s, partition.Indices)
+	p2, err := Make(s, partition.Indices)
 	assert.Equal(t, partition.Indices, p2.Indices)
 	assert.True(t, reflect.DeepEqual(partition.Cells, p2.Cells))
 
 	// Test the resulting cells the actual partition from the definition.
-	expectedPar := []Slice{
+	expectedPar := []mass.Slice{
 		{{0, 1}, {1, 2.2}},
 		{{2, 8.2}, {3, 9}},
 	}
@@ -39,15 +40,15 @@ func TestMakePartition(t *testing.T) {
 
 func TestMinSizeCells(t *testing.T) {
 	var err error
-	var s Slice
+	var s mass.Slice
 	// A partition variable.
 
-	// Test that partitioning the empty Slice returns an error.
-	_, err = DefineMinSizeCells(Slice{}, 1, 0)
+	// Test that partitioning the empty mass.Slice returns an error.
+	_, err = DefineMinSizeCells(mass.Slice{}, 1, 0)
 	assert.NotNil(t, err)
 
 	// Test that the cells can be appropriately re-sized.
-	s = Slice{
+	s = mass.Slice{
 		{nil, 1},
 		{nil, 2},
 		{nil, 2},
