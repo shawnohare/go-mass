7cc6aff11d26de4006e0569aed30d6b69e044ac6
Shawn O'Hare <ohare.shawn@gmail.com>
2015-07-08 22:27:10 -0700

    Significant refactor of partitioning.

--- partition.go
+++ partition.go
@@ -1,78 +1,153 @@
 package mass
 
 import (
-	"fmt"
+	"errors"
+	"math"
 )
 
-// Bin is an arbitrary interval of masses.  It specifies the
-// index range of data whose mass falls in the bin as well as
-// mass endpoints.  Whether the bin is open, closed, or clopen
-// depends on context and is not encoded explicitly.
-type Bin struct {
-	MinEdge  float64 // left endpoint of interval
-	MaxEdge  float64 // right endpoint of interval
-	Width    float64 // length of subinterval defining the bin
-	MinIndex int     // index of initial element in bin
-	MaxIndex int     // index of final element in bin
-	Type     string  // whether bin is (), [], (], [)
+// A Partition of a Collection interface into cells.
+// We always view the collection, via its natural embedding, as a discrete
+// subset of the reals.  All partition definitions are actually a family
+// of sub-interval cell partitions that induce the same discrete partition
+// over the collection.  That is, individual partition functions
+// return a list of slice points for the collection that defines
+// the partition.
+type Partition struct {
+	// Slice points that define the partition.
+	Indices []int
+	// Points on the Real line that induce the partition.
+	Points []float64
+	// The actual partition of the collection into discrete cells.
+	Cells []Slice
 }
 
-// EqualWidthBins provides the index ranges of
-// the sorted (by mass) data that fall into n := numBins
-// equal width sub-intervals over the range of the data.
-// The output is a slice of n Bin objects that encode
-func EqualWidthBins(data Interface, numBins int) ([]Bin, error) {
+// Make produces a slice of sub-collections according
+// to the input partition indices.  This function assumes that the
+// collection has been previously sorted.
+func MakePartition(c Collection, partition []int) (*Partition, error) {
 	var err error
-	var bins []Bin
-	l := data.Len()
 
-	switch {
-	case numBins < 1:
-		err = fmt.Errorf("Number of bins must be > 0.")
-	case l < 2:
-		err = fmt.Errorf("Too few data points to partition.  data.Len()=%d < 2.", l)
-	case data.Mass(0) < data.Mass(l-1):
-		err = fmt.Errorf("Data not sorted.  Maximal mass %f < minimal mass %f.", data.Mass(0), data.Mass(l-1))
-	case data.Mass(0) == data.Mass(l-1):
-		err = fmt.Errorf("Cannot create partition. Minimal mass %f = maximal mass %f.", data.Mass(0), data.Mass(l-1))
-	default:
-		min := data.Mass(0)
-		max := data.Mass(l - 1)
-		bins = make([]Bin, numBins)
-		width := (max - min) / float64(numBins)
-		rightIndex := 0
+	cells, err := makeCells(c, partition)
+	points := makePoints(c, partition)
 
-		for i := 0; i < numBins; i++ {
-			left := min + float64(i)*width
-			right := left + width
-			leftIndex := rightIndex
-
-			if i < numBins-1 {
-				for data.Mass(rightIndex) < right {
-					rightIndex++
-				}
-				bin := Bin{
-					MinEdge:  left,
-					MaxEdge:  right,
-					Width:    width,
-					MinIndex: leftIndex,
-					MaxIndex: rightIndex,
-					Type:     "[ )",
-				}
-				bins[i] = bin
-			} else {
-				// Final bin
-				bin := Bin{
-					MinEdge:  left,
-					MaxEdge:  right,
-					Width:    width,
-					MinIndex: leftIndex,
-					MaxIndex: l - 1,
-					Type:     "[  ]",
-				}
-				bins[i] = bin
-			}
-		}
+	p := &Partition{
+		Indices: partition,
+		Points:  points,
+		Cells:   cells,
 	}
-	return bins, err
+
+	return p, err
+}
+
+func makeCells(c Collection, partition []int) ([]Slice, error) {
+	if len(partition) < 2 || c.Len() < len(partition)-1 {
+		err := errors.New("Could not make cells.")
+		return nil, err
+	}
+
+	s := MakeSlice(c)
+	numCells := len(partition) - 1
+	ss := make([]Slice, numCells)
+	for i := 0; i < numCells; i++ {
+		start, end := partition[i], partition[i+1]
+		ss[i] = s[start:end]
+	}
+
+	return ss, nil
+}
+
+// makePoints a slice-index partition into
+// a sub-interval partition whose cells have maximum width.
+func makePoints(c Collection, par []int) []float64 {
+	points := make([]float64, len(par))
+	for i, idx := range par {
+		if idx == c.Len() {
+			idx = idx - 1
+		}
+		points[i] = c.Mass(idx)
+	}
+
+	return points
+}
+
+// MakeEqualWidthCells produces a Partition instance with populated cells
+// defined by the EqualWidthCells partition.
+func MakeEqualWidthCells(c Collection, n int) (*Partition, error) {
+	return MakeMinSizeCells(c, n, 0)
+}
+
+// EqualWidthCells partitions the collection into equal width subintervals.
+// It is the partial evaluation of MinSizedCells.
+func DefineEqualWidthCells(c Collection, n int) (*Partition, error) {
+	return DefineMinSizeCells(c, n, 0)
+}
+
+// MakeMinSizedCells calls MinSizedCells to obtain a partition
+// definition and then makes the specified partition.
+func MakeMinSizeCells(c Collection, n, k int) (*Partition, error) {
+	p, err := DefineMinSizeCells(c, n, k)
+	if err != nil {
+		return nil, err
+	}
+	cells, err := makeCells(c, p.Indices)
+	p.Cells = cells
+	return p, err
+}
+
+// MinSizeCells produces a partition of the previously sorted
+// input Collection into n subinterval cells each of which has at least
+// k elements. The partition tries to be as close to an equal width
+// partition as possible. In particular, when k = 0, the result is
+// an equal width partition.
+func DefineMinSizeCells(c Collection, n, k int) (*Partition, error) {
+	N := c.Len()
+	if n < 1 || k < 0 || N < n*k || N < 2 || c.Mass(N-1) == c.Mass(0) {
+		err := errors.New("Collection cannot be partitioned.")
+		return nil, err
+	}
+
+	originalWidth := (c.Mass(N-1) - c.Mass(0)) / float64(n)
+	// Define the index i of the current element we must assign to some cell.
+	i := 0
+	// The partition consists of n + 1 elements so that a pairwise traversal
+	// yields approrpiate slice points to define cells.
+	points := make([]float64, n+1)
+	points[0] = c.Mass(0)
+	points[n] = c.Mass(N - 1)
+	parIdxs := make([]int, n+1)
+	parIdxs[0] = 0
+	parIdxs[n] = N
+	for j := 1; j < n; j++ {
+		// Determine the right cell edge for an initially equal width partition.
+		// FIXME delete commented out code eventually when things stabilize.
+		// originalEdge := c.Mass(i) + (c.Mass(N-1) - c.Mass(i)/float64(n-j))
+		originalEdge := c.Mass(0) + float64(j)*originalWidth
+		i += k
+		// Set the index of element to use for determining right bin edge.
+		// We use i - 1 so that the ith element and (i-1)th element belong
+		// to the same cell if their masses are equal.
+		i2 := i - 1
+		if i2 < 0 {
+			i2 = 0
+		}
+		rightEdge := math.Max(originalEdge, c.Mass(i2))
+		points[j] = rightEdge
+
+		// Add elements into the cell until none belong or there are too
+		// few remaining elements to distribute amongst the remaining cells.
+		elemsLeft := N - i
+		elemsToDistribute := k * (n - j - 1)
+		for c.Mass(i) <= rightEdge && elemsLeft > elemsToDistribute {
+			i++
+			elemsLeft--
+		}
+		parIdxs[j] = i
+	}
+
+	def := &Partition{
+		Indices: parIdxs,
+		Points:  points,
+	}
+
+	return def, nil
 }
--- partition_test.go
+++ partition_test.go
@@ -1,15 +1,61 @@
-package partition
+package mass
 
-type Masses []float64
+import (
+	"reflect"
+	"testing"
 
-func (m Masses) Len() int {
-	return len(m)
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakePartition(t *testing.T) {
+	var err error
+	var s Slice
+	// A partition variable.
+
+	// Test a basic two cell equal width partition.
+	s = Slice{
+		{0, 1},
+		{1, 2.2},
+		{2, 8.2},
+		{3, 9},
+	}
+	partition, err := MakeMinSizeCells(s, 2, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, []int{0, 2, 4}, partition.Indices)
+	assert.Equal(t, []float64{1, 5, 9}, partition.Points)
+
+	// Ensure that Make* and Make produce the same output.
+	p2, err := MakePartition(s, partition.Indices)
+	assert.Equal(t, partition.Indices, p2.Indices)
+	assert.True(t, reflect.DeepEqual(partition.Cells, p2.Cells))
+
+	// Test the resulting cells the actual partition from the definition.
+	expectedPar := []Slice{
+		{{0, 1}, {1, 2.2}},
+		{{2, 8.2}, {3, 9}},
+	}
+	assert.True(t, reflect.DeepEqual(partition.Cells, expectedPar))
 }
 
-func (m Masses) Mass(i int) float64 {
-	return m[i]
-}
+func TestMinSizeCells(t *testing.T) {
+	var err error
+	var s Slice
+	// A partition variable.
 
-func TestEqualWidthBins(t *testing.T) {
+	// Test that partitioning the empty Slice returns an error.
+	_, err = DefineMinSizeCells(Slice{}, 1, 0)
+	assert.NotNil(t, err)
 
+	// Test that the cells can be appropriately re-sized.
+	s = Slice{
+		{nil, 1},
+		{nil, 2},
+		{nil, 2},
+		{nil, 8},
+		{nil, 9},
+		{nil, 10},
+	}
+	p, err := DefineMinSizeCells(s, 3, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, p.Indices, []int{0, 3, 4, 6})
 }
