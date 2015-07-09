686d44871606d5b1de3f596691b2f3083fcedbad
Shawn O'Hare <ohare.shawn@gmail.com>
2015-07-09 08:50:28 -0700

    Add MapPair and tweak Map functions for StatModel.

--- model.go
+++ model.go
@@ -1,15 +1,23 @@
 package mass
 
-// An Interface reprsenting a real-valued statistical model.
-type Model interface {
+// An Model interface a real-valued statistical model.
+type StatModel interface {
 	Eval(interface{}) float64
 }
 
 // Map a real-valued statistical model over a collection of input data.
-func Map(m Model, inputs []interface{}) Slice {
+func MapPairs(m StatModel, inputs []interface{}) Slice {
 	outputs := make(Slice, len(inputs))
 	for i, x := range inputs {
 		outputs.Set(i, x, m.Eval(x))
+	}
+	return outputs
+}
+
+func Map(m StatModel, inputs []interface{}) []float64 {
+	outputs := make([]float64, len(inputs))
+	for i, x := range inputs {
+		outputs[i] = m.Eval(x)
 	}
 	return outputs
 }
