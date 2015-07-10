f10858d42c0d92856b0896ce874e2c5180644dc5
Shawn O'Hare <ohare.shawn@gmail.com>
2015-07-10 14:57:20 -0700

    MakeSlice accepts interfaces.

--- slice.go
+++ slice.go
@@ -6,22 +6,45 @@ type Slice []*Pair
 
 // Implement a List and sort interface.
 func (s Slice) Get(i int) interface{}                 { return s[i].Element }
-func (s Slice) Set(i int, e interface{}, val float64) { s[i] = &Pair{e, val} }
+func (s Slice) Set(i int, e interface{}, val float64) { s[i] = NewPair(e, val) }
 func (s Slice) Val(i int) float64                     { return s[i].Value }
 func (s Slice) Len() int                              { return len(s) }
 func (s Slice) Less(i, j int) bool                    { return s.Val(i) < s.Val(j) }
 func (s Slice) Swap(i, j int)                         { s[i], s[j] = s[j], s[i] }
 
-func MakeSlice(l List) Slice {
+// MakeSlice will produce a Slice instance either from an input List
+// or []float64 slice.  If the input is type []float64, then the
+// underlying Element is an empty struct.
+func MakeSlice(l interface{}) Slice {
+
+	var s Slice
+
+	switch t := l.(type) {
+	case List:
+		s = makeSliceFromList(t)
+	case []float64:
+		s = makeSliceFromFloat64s(t)
+	}
+
+	return s
+}
+
+func makeSliceFromList(l List) Slice {
 	// Create a new Slice struct that represents the input List.
 	s := make(Slice, l.Len())
 
 	// Populate the list struct with the input List's data.
 	for i := range s {
-		s[i] = &Pair{
-			Element: l.Get(i),
-			Value:   l.Val(i),
-		}
+		s.Set(i, l.Get(i), l.Val(i))
+	}
+
+	return s
+}
+
+func makeSliceFromFloat64s(l []float64) Slice {
+	s := make(Slice, len(l))
+	for i := range l {
+		s.Set(i, struct{}{}, l[i])
 	}
 
 	return s
